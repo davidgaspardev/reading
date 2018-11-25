5e650d337c0d7485ac924e01f62e1c4fe5b212e3
David Gaspar <davidgaspar.dev@gmail.com>
2018-11-25 19:18:43 -0200

    Actions created (Posts and Comments)

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,3 +3,33 @@
  * By David Corrêa Gaspar (davidgaspar.dev@gmail.com)
  * Path: PROJECT/src/actions/index.js
  */
+const ADD_POSTS = 'ADD_POSTS';
+const ADD_COMMENT = 'ADD_COMMENT';
+
+export function addPosts({ id, timestamp, title, body, author, category, voteScore, deleted }) {
+  return {
+    type: ADD_POSTS,
+    id,
+    timestamp,
+    title,
+    body,
+    author,
+    category,
+    voteScore,
+    deleted
+  }
+}
+
+export function addComment({ id, parentId, timestamp, body, author, voteScore, deleted, parentDeleted }) {
+  return {
+    type: ADD_COMMENT,
+    id,
+    parentId,
+    timestamp,
+    body,
+    author,
+    voteScore,
+    deleted,
+    parentDeleted
+  }
+}
--- src/components/index.js
+++ src/components/index.js
@@ -3,3 +3,15 @@
  * By David Corrêa Gaspar (davidgaspar.dev@gmail.com)
  * Path: PROJECT/src/components/index.js
  */
+import React, { Component } from 'react';
+import './index.css';
+
+export default class Root extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello World!</h1>
+      </div>
+    );
+  }
+}
